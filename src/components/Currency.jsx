88168698c9fbc4130e8485528727ec504becf09e
Diego Castro <diego.castroalves1@gmail.com>
2022-11-24 01:17:24 -0300

    Alterado a forma que os valores são exibidos

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,12 +1,15 @@
 import Coin from '../coin-logo.svg';
 export const Currency = ({ currency, value }) => {
+  const replaceToComma = value.replace('.', ',');
+  const valuePattern = replaceToComma.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+
   return (
     <div className='currency'>
       <p className='currency-name'>
         <img className='currency-icon' src={Coin} alt={currency} />
         {currency}
       </p>
-      <p className='currency-value'>{value}</p>
+      <p className='currency-value'>{valuePattern}</p>
     </div>
   );
 };
